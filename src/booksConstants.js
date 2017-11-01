703325eb26d4b85899b9439318d2f2b0353db46b
Ilya Sevrugin <isevrugin@shutterstock.com>
2017-11-01 09:18:00 -0700

    Linting and updating docs

--- src/booksConstants.js
+++ src/booksConstants.js
@@ -9,7 +9,7 @@ export const SHELVES_TITLES = {
   [SHELVES[0]]: 'Currently Reading',
   [SHELVES[1]]: 'Want to Read',
   [SHELVES[2]]: 'Read',
-  [SHELVES[3]]: 'None',
+  [SHELVES[3]]: 'None'
 };
 
 export default {
